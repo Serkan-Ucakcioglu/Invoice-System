c7afe9dda8e442a5116e05d0225dbb3c1a222c6a
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-20 14:54:23 +0300

    side bar splitting

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AddInvoice from "../../Assets/AddInvoice";
+import Header from "./Header";
 import Process from "./Process";
 
 function SideBar() {
@@ -7,20 +7,7 @@ function SideBar() {
 
   return (
     <div className="w-[230px] bg-black h-screen p-5">
-      <header className="flex flex-col justify-center items-center border-b border-gray-200">
-        <img
-          className="w-[80px] h-[80px] rounded"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzF9KnRH0A3_pOqMj8exwrwCLw_G9E7rGLkA&usqp=CAU"
-          alt="invoice-image"
-        />
-        <button
-          type="button"
-          className="focus:outline-none flex flex-row  mt-4 w-3/3 text-white bg-indigo-900 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium  text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-        >
-          <AddInvoice />
-          invoice
-        </button>
-      </header>
+      <Header />
       <Process />
     </div>
   );
