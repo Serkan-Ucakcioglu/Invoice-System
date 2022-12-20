22bd025640277f50ed3f64b4f94f05e81dd7f335
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-20 15:18:23 +0300

    invoice add button css fix

--- src/Components/SideBar/Header.jsx
+++ src/Components/SideBar/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
       />
       <button
         type="button"
-        className="focus:outline-none flex flex-row  mt-4 w-3/3 text-white bg-indigo-900 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium  text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+        className="focus:outline-none flex flex-row  mt-4 w-3/3 text-white bg-indigo-900 hover:bg-purple-800 font-medium text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700"
       >
         <AddInvoice />
         invoice
