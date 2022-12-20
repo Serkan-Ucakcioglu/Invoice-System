c69e384689d319e537c1b632b66e28cba1e6f288
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-20 14:52:43 +0300

    add invoice create

---
+++ src/Assets/AddInvoice.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AddInvoice() {
+  return (
+    <svg
+      className="w-5 h-5 mr-2"
+      fill="#fff"
+      viewBox="0 0 200 200"
+      data-name="Layer 1"
+      id="Layer_1"
+    >
+      <title />
+      <path d="M100,15a85,85,0,1,0,85,85A84.93,84.93,0,0,0,100,15Zm0,150a65,65,0,1,1,65-65A64.87,64.87,0,0,1,100,165Zm30-72.5H110v-20a10,10,0,0,0-20,0v20H70a10,10,0,0,0,0,20H90v20a10,10,0,0,0,20,0v-20h20a10,10,0,0,0,0-20Z" />
+    </svg>
+  );
+}
+
+export default AddInvoice;
