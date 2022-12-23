848e2be922509554d29d040e5ce748514f3722b5
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-23 13:23:41 +0300

    update

--- src/Features/Invoice/Product/Product.jsx
+++ src/Features/Invoice/Product/Product.jsx
@@ -3,12 +3,19 @@ import { useSelector } from "react-redux";
 import { selectedProductList } from "../../invoiceSlice";
 import ProductList from "./ProductList";
 
-function Product() {
+function Product({ watch, register }) {
   const productList = useSelector(selectedProductList);
   return (
     <>
       {productList?.map((input) => {
-        return <ProductList key={input?.id} id={input?.id} />;
+        return (
+          <ProductList
+            key={input?.id}
+            id={input?.id}
+            watch={watch}
+            register={register}
+          />
+        );
       })}
     </>
   );
