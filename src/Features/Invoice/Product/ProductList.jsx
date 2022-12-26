d811bb3fd3a14c45eb023a3c81d281c39a4853ac
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-12-26 23:38:29 +0300

    clears

--- src/Features/Invoice/Product/ProductList.jsx
+++ src/Features/Invoice/Product/ProductList.jsx
@@ -1,31 +1,14 @@
-import { useDispatch } from "react-redux";
-import { updateProduct } from "../../invoiceSlice";
 import { useFieldArray, useFormContext } from "react-hook-form";
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import AddBtn from "./AddBtn";
 import ProductInput from "./ProductInput/ProductInput";
 
-function ProductList({ id }) {
-  const dispatch = useDispatch();
-  const { watch, control } = useFormContext();
+function ProductList() {
+  const { control } = useFormContext();
   const { fields, append, remove } = useFieldArray({
     control,
     name: "test",
   });
-
-  const total = watch("test")?.reduce((acc, arr) => {
-    return acc + arr.quantity * arr.price;
-  }, 0);
-
-  const onSubmit = (data) => {
-    dispatch(updateProduct({ id, total }));
-  };
-
-  useEffect(() => {
-    console.log("calıstı");
-    onSubmit();
-  }, [total]);
-
   useEffect(() => {
     append({ product: "", quantity: 0, price: 0 });
   }, []);
